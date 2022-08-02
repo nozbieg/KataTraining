b9637a1f91faed96aa797451237cc6d33a08f9c8
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2022-08-02 19:04:48 +0200

    Letters Counter

---
+++ KataTraining/LettersCounter/LettersCounterKata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace KataTraining.LettersCounter
+{
+    public static class LettersCounterKata
+    {
+        public static Dictionary<char, int> CountLetters(string input)
+        {
+
+
+            var lettersDictionary = new Dictionary<char, int>();
+            foreach (var letter in input.ToLower())
+            {
+
+                if (!lettersDictionary.ContainsKey(letter))
+                {
+                    lettersDictionary.Add(letter, 1);
+                }
+                else
+                {
+                    lettersDictionary[letter]++;
+                }
+            }
+
+            return lettersDictionary;
+        }
+    }
+}
---
+++ KataTrainingTest/LettersCounter/LettersCounterTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using KataTraining.LettersCounter;
+
+namespace KataTrainingTest.LettersCounter
+{
+    public class LettersCounterTest
+    {
+
+        [Fact]
+        public void LettersCounter_Test()
+        {
+            var input = "Ala ma małego fajnego kota, a Adek ma na niego alergie";
+            var funcResult = LettersCounterKata.CountLetters(input);
+            funcResult['a'].Should().Be(11);
+        }
+
+    }
+}
