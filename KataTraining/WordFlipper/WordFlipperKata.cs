2ff85ed85587be1d28d5f839cdb026d21863f565
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2022-08-02 19:25:48 +0200

    word fliping

---
+++ KataTraining/WordFlipper/WordFlipperKata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace KataTraining.WordFlipper
+{
+    public static class WordFlipperKata
+    {
+        public static string FlipWordSequence(string input)
+        {
+
+            return string.Join(' ', input.Split(' ').Where(x => !string.IsNullOrEmpty(x)).Reverse().ToArray());
+        }
+    }
+}
---
+++ KataTrainingTest/WordFlipper/WordFlipperTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using KataTraining.WordFlipper;
+
+namespace KataTrainingTest.WordFlipper
+{
+    public class WordFlipperTest
+    {
+
+        [Fact]
+        public void WordFlipper_Success_Test()
+        {
+            var input = "jeden dwa trzy cztery";
+
+            var funcResult = WordFlipperKata.FlipWordSequence(input);
+
+            var output = "cztery trzy dwa jeden";
+
+            funcResult.Should().Be(output);
+
+        }
+    }
+}
