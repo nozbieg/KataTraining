3ff72c94187f5b7872b4189ad8550d2c2c07fb4d
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2022-08-02 19:49:24 +0200

    Cytcle finder

---
+++ KataTraining/CycleFinder/CylceFinderKata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace KataTraining.CycleFinder
+{
+    public static class CylceFinderKata
+    {
+        public static bool DetectCycleInArray(int[] array)
+        {
+            int harePos = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                var tortoiseValue = array[i];
+
+                if (harePos >= array.Length)
+                {
+                    harePos = harePos - array.Length;
+                }
+                var hareValue = array[i];
+                harePos += 2;
+                if (tortoiseValue == hareValue)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ KataTrainingTest/CycleFinder/CycleFinderTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using KataTraining.CycleFinder;
+
+namespace KataTrainingTest.CycleFinder
+{
+    public class CycleFinderTest
+    {
+
+        [Fact]
+        public void CycleFinder_Success_Test()
+        {
+            var input = new[] { 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3 };
+            var funcResult = CylceFinderKata.DetectCycleInArray(input);
+
+            funcResult.Should().BeTrue();
+
+        }
+
+
+        [Fact]
+        public void CycleFinder_Fail_Test()
+        {
+            var input = new[] { 1, 2, 3, 4, 5 };
+            var funcResult = CylceFinderKata.DetectCycleInArray(input);
+
+            funcResult.Should().BeFalse();
+
+        }
+
+        [Theory]
+        [InlineData(new[] { 9, 8, 7, 6, 1, 2, 3, 4, 9, 8, 7, 6 })]
+        [InlineData(new[] { 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3 })]
+        [InlineData(new[] { 1, 75, 53, 2, 5, 1, 75, 53, 2, 5, 1, 12, 32, 1, 75, 53, 2, 1, 75, 53 })]
+        [InlineData(new[] { 3, 2, 3, 3, 3, 3, 1, 2, 3, 1, 2, 3 })]
+        [InlineData(new[] { 9, 8, 7, 6, 9, 8, 7, 6, 9, 8, 7, 6 })]
+        public void CycleFinder_Theory_Test(int[] input)
+        {
+            var funcResult = CylceFinderKata.DetectCycleInArray(input);
+
+            funcResult.Should().BeTrue();
+
+        }
+    }
+}
