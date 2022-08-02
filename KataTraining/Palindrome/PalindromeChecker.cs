5b4ee5daf33f954978f756782a3daf084b9ba53b
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2022-08-02 18:56:24 +0200

    palindromes

---
+++ KataTraining/Palindrome/PalindromeChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace KataTraining.Palindrome
+{
+    public static class PalindromeChecker
+    {
+        public static bool CheckPalindrome(string input)
+        {
+            var output = new string(input.ToCharArray().Reverse().ToArray());
+
+            if (input == output)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
---
+++ KataTrainingTest/Palindrome/PalindromeCheckerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using KataTraining.Palindrome;
+
+namespace KataTrainingTest.Palindrome
+{
+    public class PalindromeCheckerTest
+    {
+        [Fact]
+        public void BasicPalindromeSuccessTest()
+        {
+            var input = "kajak";
+            var success = PalindromeChecker.CheckPalindrome(input);
+            success.Should().Be(true);
+        }
+
+        [Fact]
+        public void BasicPalindromeFailTest()
+        {
+            var input = "asdfghjiklkasd";
+            var success = PalindromeChecker.CheckPalindrome(input);
+            success.Should().Be(false);
+        }
+
+
+        [Theory]
+        [InlineData("kajak")]
+        [InlineData("ada")]
+        [InlineData("123321")]
+
+        public void PalindromeMulti_SuccessTest(string input)
+        {
+
+            var success = PalindromeChecker.CheckPalindrome(input);
+            success.Should().Be(true);
+        }
+    }
+}
